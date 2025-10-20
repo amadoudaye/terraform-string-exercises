dea6a60b95b9e167d0c0eb91462be80bc2494690
amadoudaye <amadoudaye@gmail.com>
2025-10-20 23:39:23 +0000

    outputs

---
+++ outputs.tf
@@ -0,0 +1,42 @@
+# -------------------
+# Outputs
+# -------------------
+output "company_department" {
+  value = local.company_department
+}
+
+output "countries_upper" {
+  value = local.countries_upper
+}
+
+output "cities_lower" {
+  value = local.cities_lower
+}
+
+output "department_length" {
+  value = local.department_length
+}
+
+output "company_first_two" {
+  value = local.company_first_two
+}
+
+output "cities_joined" {
+  value = local.cities_joined
+}
+
+output "skills_list" {
+  value = local.skills_list
+}
+
+output "department_replaced" {
+  value = local.department_replaced
+}
+
+output "intro" {
+  value = local.intro
+}
+
+output "city_check" {
+  value = local.city_check
+}
